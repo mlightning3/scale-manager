5977d613d9ca57fd61600f686da0c7e8986837da
Matt Kohls <mattkohls13@gmail.com>
2018-06-01 16:01:51 -0400

    Adding in code for communicating with Scrybe

--- scale.py
+++ scale.py
@@ -2,7 +2,7 @@
 
 ## Scale Manager
 #
-# Reads from a SparkFun OpenScale board and communicates that back to the Scribe system
+# Reads from a SparkFun OpenScale board and communicates that back to the Scrybe system
 #
 #
 # Authors: Matt Kohls
@@ -10,11 +10,15 @@
 
 # Imports
 import serial
-import sys, getopt, time
+import sys
+import getopt
+import time
+import socket
 
 ## Sets Up Scale
 #
 # @param serialport is a serial object that is connected to the serial port the scale is connected on
+# @param hostconnection is a socket that is connected to the Scrybe server
 #
 # Handles setting the board up after being turned on (taring and calibrating the scale)
 # Asks the user for the state of the scale during the process
@@ -22,35 +26,35 @@ import sys, getopt, time
 # Steps involved:
 # 1. Remove all weight from scale and tare sensor
 # 2. Put a known weight on scale and calibrate to that value
-def setup_scale(serialport):
+def setup_scale(serialport, hostconnection):
     print('Setting up scale...')
     temp = ''
     while temp != 'Readings:\r\n'.encode('latin-1'):
         temp = serialport.readline() # Repeat until we get to the line before readings from sensor
-        #print(temp)
     line = serialport.readline()
-    #print(line)
     time.sleep(.1)
     serialport.write('x'.encode('latin-1')) # Get into menu to calibrate board
     temp = serialport.read()
-    #print(temp)
     while temp != '>'.encode('latin-1'):
         temp = serialport.readline() # Repeat till we get to prompt
-        #print(temp)
         if temp == ''.encode('latin-1'):
             serialport.write('x'.encode('latin-1'))
-    input('Remove all weight from scale. Press enter to continue...')
+
+    response = ''
+    while response != 'yes':
+        hostconnection.send('is scale empty'.encode('UTF-8'))
+        response = hostconnection.recv(1024)
+
+    #input('Remove all weight from scale. Press enter to continue...')
     serialport.write('1'.encode('latin-1'))
     temp = ' '
     while temp != '>'.encode('latin-1'):
         temp = serialport.readline()  # Repeat till we get to prompt
-        #print(temp)
         if temp == ''.encode('latin-1'):
             serialport.write('x'.encode('latin-1'))
     # TODO: calibrate with known weight knownweight = input('Put known weight on scale. Enter value: ')
     serialport.write('x'.encode('latin-1'))
     line = serialport.readline()
-    #print(line)
 
 ## Read Sensor
 #
@@ -66,12 +70,10 @@ def read(serialport):
     while line == ''.encode('latin-1'):
         serialport.write('t'.encode('latin-1'))
         line = serialport.readline()
-        #print(line)
     linedata = line.split(','.encode('latin-1'))
     linedata.pop()
-    #print(linedata)
-    time, weight, units, temperature = linedata
-    time = time.decode('utf-8')
+    board_time, weight, units, temperature = linedata
+    board_time = board_time.decode('utf-8')
     weight = weight.decode('utf-8')
     units = units.decode('utf-8')
     temperature = temperature.decode('utf-8')
@@ -79,36 +81,51 @@ def read(serialport):
 
 ## Send Data To Scribe
 #
+# @param hostconnection is a socket that is connected to the Scrybe server
+#
 # Updates Scribe with the patient's weight
-def send_to_scribe():
+def send_to_scrybe(value, hostconnection):
     print('...sending...')
+    hostconnection.send(bytes(value, 'UTF-8'))
 
 
 ## Main
 #
 # Sets up the program/hardware, and then goes into loop of reading from scale and sending values
 def main(argv):
-    # Global Variables
-    ser = 0
+    # Important Variables and their defaults
+    ser = None
+    host_socket = None
     tty = '/dev/ttyUSB0'
     baud = 9600
+    host_ip = '127.0.0.1'
+    port = 8089
 
     # Command Line Parsing
     try:
-        opts, args = getopt.getopt(argv, "ht:b", ["tty=", "baud="])
+        opts, args = getopt.getopt(argv, "ht:b:i:p", ["tty=", "baud=", "ip=", "port="])
     except getopt.GetoptError:
-        print ('scale.py -t <tty device> -b <baud>')
+        print ('scale.py -i <ip address>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == "-h":
             print('Scale Manager')
-            print('Sets-up and reads values from scale and sends to the Scribe system')
-            print ('Useage: scale.py -t <tty device> -b <baud>')
+            print('Sets-up and reads values from scale and sends to the Scrybe system')
+            print('\nUseage: scale.py -i <ip address>')
+            print('Optional commands:')
+            print('-t <tty device> or --tty=<tty device>    : sets the port to listen to the scale on')
+            print('-b <baud> or --baud=<baud>               : sets the baud rate to listen to the scale')
+            print('-i <ip address> or --ip=<ip address>     : sets the ip address of the Scrybe host')
+            print('-p <port number> or --port=<port number> : sets the port number of the Scrybe host')
             sys.exit()
         elif opt in ("-t", "--tty"):
             tty = arg
         elif opt in ("-b", "--baud"):
             baud = arg
+        elif opt in ("-i", "--ip"):
+            host_ip = arg
+        elif opt in ("-p", "--port"):
+            port = arg
 
     print('Opening serial port', tty, 'at', baud, 'baud...')
     try:
@@ -122,13 +139,21 @@ def main(argv):
     else:
         print('Serial port opened')
 
-    # When we get the signal the scale is ready for setup
-    setup_scale(ser)
+    print('Connecting to Scrybe at', host_ip, 'on port', port, '...')
+    host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_socket.connect((host_ip, port)) # Defaults to localhost at 8089
+    # TODO: error handling
+    host_socket.send('scale connecting'.encode('UTF-8'))
+    msg = host_socket.recv(1024)
+    print('Connected to Scrybe')
+
+    setup_scale(ser, host_socket)
 
     # When Scribe wants a reading
     print('Weight:', read(ser))
-    send_to_scribe()
+    send_to_scrybe(read(ser))
 
+    host_socket.close()
     ser.close()
 
 # Sets up things when run from the commandline
